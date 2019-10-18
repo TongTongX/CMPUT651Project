ec7165dfd4fe2710f9a3b367e157d5bdec53f15f
sluxsr <roy996812@gmail.com>
2019-10-18 16:13:52 -0600

    modifiled dataloader to OO

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,52 +1,53 @@
 import numpy as np
 import re
 
-def readCSV(filename):
-    f = open(filename, 'r')
-    dataset = []
-    for line in f:
-        line=line[:-1]
-        row = re.split(' ,|,',line)
-        dataset.append(row)
+class DataLoader:
+    def __init__(self):
+        self.PATTERN = ' ,|,'
 
-        # for testing
-        # print("")
-        # print("\n".join(row))
-        # input()
-        # # if len(row)<9:
-        # #     print("")
-        # #     print("\n".join(row))
-        # #     input()
-    return np.asarray(dataset)
+    def readCSV(self, filename):
+        f = open(filename, 'r')
+        dataset = []
+        for line in f:
+            line=line[:-1]
+            row = re.split(self.PATTERN,line)
+            dataset.append(row)
 
-def preprocess_label(dataset):
+            # for testing
+            # print("")
+            # print("\n".join(row))
+            # input()
+            # # if len(row)<9:
+            # #     print("")
+            # #     print("\n".join(row))
+            # #     input()
+        return np.asarray(dataset)
+        
+    # data has been processed, this method won't be called any longer
+    def preprocess_label(self,dataset):
+        f = open('./data_output_multilabel.csv', 'w')
 
-    f = open('./data_output_multilabel.csv', 'w')
+        VALID_LABELS = {
+            'humour': ['not_funny', 'funny', 'very_funny', 'hilarious'],
+            'sarcasm': ['not_sarcastic', 'general', 'twisted_meaning', 'very_twisted'],
+            'offensive': ['not_offensive', 'slight', 'very_offensive', 'hateful_offensive'],
+            'motivational': ['not_motivational', 'motivational'],
+            'overall_sentiment': ['very_negative', 'negative', 'neutral', 'positive', 'very_positive']}
 
-    new_dataset = dataset
+        for data in dataset:
+            multilabel_onehot = [0, 0, 0, 0, 0]
 
-    VALID_LABELS = {
-        'humour': ['not_funny', 'funny', 'very_funny', 'hilarious'],
-        'sarcasm': ['not_sarcastic', 'general', 'twisted_meaning', 'very_twisted'],
-        'offensive': ['not_offensive', 'slight', 'very_offensive', 'hateful_offensive'],
-        'motivational': ['not_motivational', 'motivational'],
-        'overall_sentiment': ['very_negative', 'negative', 'neutral', 'positive', 'very_positive']}
+            multilabel_onehot[0] = str(VALID_LABELS['humour'].index(data[4]))
+            multilabel_onehot[1] = str(VALID_LABELS['sarcasm'].index(data[5]))
+            multilabel_onehot[2] = str(VALID_LABELS['offensive'].index(data[6]))
+            multilabel_onehot[3] = str(VALID_LABELS['motivational'].index(data[7]))
+            multilabel_onehot[4] = str(VALID_LABELS['overall_sentiment'].index(data[8]))
 
-    for data in dataset:
-        multilabel_onehot = [0, 0, 0, 0, 0]
-
-        multilabel_onehot[0] = str(VALID_LABELS['humour'].index(data[4]))
-        multilabel_onehot[1] = str(VALID_LABELS['sarcasm'].index(data[5]))
-        multilabel_onehot[2] = str(VALID_LABELS['offensive'].index(data[6]))
-        multilabel_onehot[3] = str(VALID_LABELS['motivational'].index(data[7]))
-        multilabel_onehot[4] = str(VALID_LABELS['overall_sentiment'].index(data[8]))
-
-        data[4:9] = multilabel_onehot
-
-        f.write(",".join(data)+"\n")
+            data[4:9] = multilabel_onehot
+            f.write(",".join(data)+"\n")
 
 if __name__ == "__main__":
     # readCSV("data_7000.csv")
     # dataset = readCSV("data_7000_new.csv")
-    # preprocess_label(dataset)
-    dataset = readCSV("data_output_multilabel.csv")
+    dataloader = DataLoader()
+    dataset = dataloader.readCSV("data_output_multilabel.csv")

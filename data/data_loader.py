7b9c84779ccc575c24e3eef1f24ce65721bb7ebb
sluxsr <roy996812@gmail.com>
2019-10-18 15:27:49 -0600

    dataloader

---
+++ data/data_loader.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import re
+# data = """part 1;"this is ; part 2;";'this is ; part 3';part 4;this "is ; part" 5"""
+
+
+def readCSV(filename):
+    f = open(filename, 'r')
+    dataset = []
+    for line in f:
+        line=line[:-1]
+        row = re.split(' ,|,',line)
+        dataset.append(row)
+
+
+        # for testing
+        # print("")
+        # print("\n".join(row))
+        # input()
+        # # if len(row)<9:
+        # #     print("")
+        # #     print("\n".join(row))
+        # #     input()
+    return np.asarray(dataset)
+
+if __name__ == "__main__":
+    # readCSV("data_7000.csv")
+    dataset = readCSV("prob_test.csv")

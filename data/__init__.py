402f446ac79a4857c5f4684f5825747f607c898c
sluxsr <roy996812@gmail.com>
2019-10-25 14:29:16 -0600

    .

--- data/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from .meme_dataset import MemeDataset
-from .meme_transforms import ResizeSample, ToTensorSample, NormalizeSample
--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from . import dataset_script
-from . import transforms_script
-from . import dataloader_script
-from . import random_classify

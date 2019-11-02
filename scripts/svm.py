059a68c7d6514ba523f4d2340bc5efc6720bd60d
sluxsr <roy996812@gmail.com>
2019-11-02 17:44:06 -0600

    svm

--- scripts/svm.py
+++ scripts/svm.py
@@ -16,20 +16,12 @@ from sklearn.metrics import classification_report, confusion_matrix
 from data.meme_dataset import MemeDataset
 from data.meme_transforms import ResizeSample, ToTensorSample, NormalizeSample
 
-# def readData(fileloc):
-#     dataset = pd.read_csv(fileloc)
-#     # to be edited: 
-#     X = dataset.drop('Class', axis=1)
-#     y = dataset['Class']
-
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
-#     return X_train, X_test, y_train, y_test
-
 class SVM_Classifier:
     def __init__(self, _kernel, _degree=None):
         # (kernal, degree) can be: ('linear'), ('poly', 6), ('rbf'), ('sigmoid')
         self.kernel = _kernel
         self.degree = _degree
+        self.label_name = ['humour_int','sarcasm_int','offensive_int','motivational_int','overall_sentiment_int']
         if _degree != None:
             self.svclassifier = SVC(kernel=_kernel, degree=_degree)
         else:
@@ -51,18 +43,46 @@ class SVM_Classifier:
                 csv_file=os.path.join(os.getcwd(), '../data/data_7000_new.csv'),
                 image_dir = os.path.join(os.getcwd(), '../data/memotion_analysis_training_data/data_7000/'),
                 transform=data_transform)
+        return dataset
 
-        dataloader = DataLoader(dataset=dataset, batch_size=1,
+    def sample2data(self,sample, batch_size):
+
+        flat_img_list = np.asarray([np.asarray(torch.flatten(x)) for x in sample['image']])
+        image_name_list = np.asarray(sample['image_name'])
+        corrected_text = sample['corrected_text']
+        ocr_extracted_text = sample['ocr_extracted_text']
+        text_list = corrected_text.copy()
+        for x in range(len(text_list)):
+            if (text_list[x]) == 'nan':
+                text_list[x] = ocr_extracted_text[x]
+        text_list = np.asarray(text_list)
+        y = list()
+        for label_type in self.label_name:
+            y.append(np.asarray(sample[label_type]))
+        y=np.asarray(y).T
+
+        return flat_img_list, text_list, y 
+
+    def train(self, dataset, batch_size):
+
+        tr_split_idx = int(0.8*len(dataset)) // batch_size
+        dataloader = DataLoader(dataset=dataset, batch_size=batch_size,
             shuffle=True, num_workers=0)
-
+        
         for i_batch, sample in enumerate(dataloader):
-            print(i_batch)
-            print(len(torch.flatten(sample['image'])))
-            flat_img = torch.flatten(sample['image'])
-            break
+            flat_img_batch, text_batch, y_batch = self.sample2data(sample,batch_size)
 
-    def train(self, X_train, y_train):
-        self.svclassifier.fit(X_train, y_train)
+            # print(i_batch)
+            # print(sample)
+            # print(len(torch.flatten(sample['image'])))
+            # print(flat_img_batch)
+            print(y_batch)
+
+
+            if i_batch>1:
+                break
+
+        # self.svclassifier.fit(X_train, y_train)
         return 
 
     def predictOnTest(self, X_test, y_test):
@@ -73,5 +93,7 @@ class SVM_Classifier:
         
 if __name__ == "__main__":
     svm = SVM_Classifier('linear')
-    svm.readData('trial')
+    dataset = svm.readData('trial')
+    svm.train(dataset,3)
+    
         

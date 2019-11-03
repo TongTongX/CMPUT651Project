8db4ad75056da804d68af5bba6656a69f9a13f5e
sluxsr <roy996812@gmail.com>
2019-11-02 19:39:59 -0600

    svm

--- scripts/svm.py
+++ scripts/svm.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -17,11 +18,13 @@ from data.meme_dataset import MemeDataset
 from data.meme_transforms import ResizeSample, ToTensorSample, NormalizeSample
 
 class SVM_Classifier:
-    def __init__(self, _kernel, _degree=None):
+    def __init__(self, _kernel, _ytype, _degree=None):
         # (kernal, degree) can be: ('linear'), ('poly', 6), ('rbf'), ('sigmoid')
         self.kernel = _kernel
         self.degree = _degree
-        self.label_name = ['humour_int','sarcasm_int','offensive_int','motivational_int','overall_sentiment_int']
+        self.ytype = _ytype
+        self.txt_emb_dict = dict()
+        self.label_name_dict = {'humour_int':0,'sarcasm_int':1,'offensive_int':2,'motivational_int':3,'overall_sentiment_int':4}
         if _degree != None:
             self.svclassifier = SVC(kernel=_kernel, degree=_degree)
         else:
@@ -45,55 +48,63 @@ class SVM_Classifier:
                 transform=data_transform)
         return dataset
 
+    def readTxtEmb(self,filename):
+        path = "../data/"
+        self.PATTERN = ' ,|,'
+        f = open(path+filename)
+        for line in f:
+            line = line[:-1]
+            row = re.split(self.PATTERN,line)
+            imgname = row[0]
+            emb = row[10:]
+            emb[0],emb[-1] = emb[0][1:], emb[-1][:-1]
+            emb = [float(x) for x in emb]
+            self.txt_emb_dict[imgname] = emb
+
     def sample2data(self,sample, batch_size):
 
         flat_img_list = np.asarray([np.asarray(torch.flatten(x)) for x in sample['image']])
         image_name_list = np.asarray(sample['image_name'])
-        corrected_text = sample['corrected_text']
-        ocr_extracted_text = sample['ocr_extracted_text']
-        text_list = corrected_text.copy()
-        for x in range(len(text_list)):
-            if (text_list[x]) == 'nan':
-                text_list[x] = ocr_extracted_text[x]
-        text_list = np.asarray(text_list)
+        txt_list = np.asarray([self.txt_emb_dict[x] for x in image_name_list])
         y = list()
-        for label_type in self.label_name:
+        for label_type in self.label_name_dict.keys():
             y.append(np.asarray(sample[label_type]))
-        y=np.asarray(y).T
 
-        return flat_img_list, text_list, y 
+        y = np.asarray(y[self.label_name_dict[self.ytype]]).T
+        X = np.append(flat_img_list, txt_list, axis=1)
+        return X, y 
 
-    def train(self, dataset, batch_size):
-
-        tr_split_idx = int(0.8*len(dataset)) // batch_size
+    def splitData(self, dataset, batch_size):
         dataloader = DataLoader(dataset=dataset, batch_size=batch_size,
-            shuffle=True, num_workers=0)
-        
+            shuffle=False, num_workers=0)
+        X, y = list(),list()
+         
         for i_batch, sample in enumerate(dataloader):
-            flat_img_batch, text_batch, y_batch = self.sample2data(sample,batch_size)
+            X_batch, y_batch = self.sample2data(sample,batch_size) 
+            if type(y) == list:
+                X = np.asarray(X_batch)
+                y = np.asarray(y_batch)
+            else:
+                X = np.append(X,X_batch, axis=0)
+                y = np.append(y,y_batch, axis=0)
+        X_train, y_train, X_test,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+        print(y_train.shape,y_test.shape)
+        return X_train, y_train, X_test,y_test
 
-            # print(i_batch)
-            # print(sample)
-            # print(len(torch.flatten(sample['image'])))
-            # print(flat_img_batch)
-            print(y_batch)
+    def train(self, X_train, y_train):
+        self.svclassifier.fit(X_train, y_train)
 
-
-            if i_batch>1:
-                break
-
-        # self.svclassifier.fit(X_train, y_train)
-        return 
-
-    def predictOnTest(self, X_test, y_test):
+    def test(self, X_test, y_test):
         y_pred = self.svclassifier.predict(X_test)
-        print(confusion_matrix(y_test,y_pred))
-        print(classification_report(y_test,y_pred))
-        return
+        print(confusion_matrix(y_batch,y_pred))
+        print(classification_report(y_batch,y_pred))
         
 if __name__ == "__main__":
-    svm = SVM_Classifier('linear')
+    svm = SVM_Classifier('linear','humour_int')
+    svm.readTxtEmb("semeval-2020_trialdata/data1_textEmbs.csv")
     dataset = svm.readData('trial')
-    svm.train(dataset,3)
+    X_train, y_train, X_test,y_test = svm.splitData(dataset, 32)
+    svm.train(X_train,y_train)
+    svm.test(X_test,y_test)
     
         

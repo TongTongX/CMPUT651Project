a6d212bdb5202b610938967b7951124f7430cb47
sluxsr <roy996812@gmail.com>
2019-11-20 15:39:45 -0700

    glove cnn

--- scripts/glovecnn.py
+++ scripts/glovecnn.py
@@ -120,8 +120,7 @@ def binary_accuracy(preds, y):
     """
     Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
     """
-
-    #round predictions to the closest integer
+    #round predictions to the closest int
     rounded_preds = torch.round(torch.sigmoid(preds))
     correct = (rounded_preds == y).float() #convert into float for division 
     acc = correct.sum() / len(correct)
